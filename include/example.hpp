9e43e15f0acdde68e55e77420ff30a863a2c81fb
sasha16613 <nikolay.sasha@mail.ru>
2022-06-16 13:23:32 +0300

    ~

--- include/example.hpp
+++ include/example.hpp
@@ -1,8 +1,106 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2022 nikolay.sasha@mail.ru
+//
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_INCLUDE_HPP_
+#define INCLUDE_INCLUDE_HPP_
 
-auto example() -> void;
+#include <utility>
+#include <vector>
 
-#endif // INCLUDE_EXAMPLE_HPP_
+struct Power {
+  float num = 60;
+};
+
+class Element {
+ public:
+  [[nodiscard]] virtual float CalculateResistance(const Power&) const = 0;
+  virtual ~Element() = default;
+};
+
+class GroupConnections : public Element {
+ protected:
+  std::vector<Element*> elements;
+
+ public:
+  void addElements(Element* element) { elements.push_back(element); }
+};
+
+class SequentialConnections : public GroupConnections {
+ public:
+  [[nodiscard]] float CalculateResistance(const Power& p) const override {
+    float sum = 0.;
+    for (Element* element : elements) {
+      sum += element->CalculateResistance(p);
+    }
+    return sum;
+  }
+};
+
+class ParallelConnections : public GroupConnections {
+ public:
+  [[nodiscard]] float CalculateResistance(const Power& p) const override {
+    float invertedSum = 0.;
+    for (Element* element : elements) {
+      invertedSum += 1 / element->CalculateResistance(p);
+    }
+    return 1 / invertedSum;
+  }
+};
+class Combined : public Element {
+ private:
+  ParallelConnections pc;
+
+ public:
+  explicit Combined(ParallelConnections rhs) {
+    this->pc = std::move(rhs);
+  }
+
+  [[nodiscard]] float CalculateResistance([[maybe_unused]]
+                                          const Power& p) const final {
+    return pc.CalculateResistance(p);
+  }
+};
+
+class Resistor : public Element {
+ private:
+  float resistance;
+
+ public:
+  explicit Resistor(float rhs) { resistance = rhs; }
+
+  [[nodiscard]] float CalculateResistance(
+      [[maybe_unused]] const Power& p) const final {
+    return resistance;
+  };
+};
+
+class Capacitor : public  Element {
+ private:
+  float capacity;
+
+ public:
+  explicit Capacitor(float rhs) {
+    capacity = rhs;
+  }
+
+  [[nodiscard]] float CalculateResistance([[maybe_unused]]
+                                          const Power& p) const final {
+    return 1. / (2. * 3.14 * p.num * capacity);
+  }
+};
+class Inductor : public Element {
+ private:
+  float ind;
+
+ public:
+  explicit Inductor(float rhs) {
+    ind = rhs;
+  }
+
+  [[nodiscard]] float CalculateResistance([[maybe_unused]] const Power& p)
+      const final {
+    return (p.num * ind);
+  }
+};
+
+#endif  // INCLUDE_INCLUDE_HPP_
--- tests/example.cpp
+++ tests/example.cpp
@@ -1,16 +1,99 @@
-// Copyright 2021 Your Name <your_email>
-
-#include <stdexcept>
-#include <thread>
+// Copyright 2022 nikolay.sasha@mail.ru
+//
 
 #include <gtest/gtest.h>
 
 #include <example.hpp>
 
-TEST(Example, EmptyTest) {
-    EXPECT_THROW(example(), std::runtime_error);
+TEST(Resistor, CalculatesResistance) {
+  float resistance = 27.;
+
+  Resistor r(resistance);
+  Power p;
+
+  float actualResistance = r.CalculateResistance(p);
+
+  EXPECT_EQ(resistance, actualResistance);
 }
 
-TEST(DISABLED_Snapshot, Speen) {
-  for (;;) std::this_thread::yield();
+TEST(Resistor, SenqualConnection) {
+  float resistance1 = 27.;
+  float resistance2 = 33.;
+
+  auto* r1 = new Resistor(resistance1);
+  auto* r2 = new Resistor(resistance2);
+  Power p;
+
+  SequentialConnections c;
+  c.addElements(r1);
+  c.addElements(r2);
+
+  float actualResistance = c.CalculateResistance(p);
+
+  EXPECT_FLOAT_EQ(60., actualResistance);
+  delete r1;
+  delete r2;
+}
+
+TEST(Resistor, ParallelConnection) {
+  float resistance1 = 27.;
+  float resistance2 = 33.;
+
+  auto* r1 = new Resistor(resistance1);
+  auto* r2 = new Resistor(resistance2);
+  Power p;
+
+  ParallelConnections c;
+  c.addElements(r1);
+  c.addElements(r2);
+  float actualResistance = c.CalculateResistance(p);
+
+  EXPECT_FLOAT_EQ(14.85, actualResistance);
+  delete r1;
+  delete r2;
+}
+
+TEST(Capacitor, CalculatesResistance) {
+  float capacity = 27.;
+
+  Capacitor r(capacity);
+  Power p;
+
+  float actualResistance = r.CalculateResistance(p);
+
+  EXPECT_EQ((float)(1 / (2 * 3.14 * 60 * capacity)), actualResistance);
+}
+
+TEST(Inductor, CalculatesResistance) {
+  float induction = 27.;
+
+  Inductor r(induction);
+  Power p;
+
+  float actualResistance = r.CalculateResistance(p);
+
+  EXPECT_EQ(60 * induction, actualResistance);
+}
+
+TEST(All, Combination) {
+  float capasitor = 12.;
+  float resistor = 27.;
+  float inductor = 7.;
+  auto* c1 = new Capacitor(capasitor);
+  auto* r1 = new Resistor(resistor);
+  auto* i1 = new Inductor(inductor);
+  Power p;
+
+  SequentialConnections sc;
+  sc.addElements(c1);
+  sc.addElements(r1);
+  ParallelConnections pc;
+  pc.addElements(i1);
+  auto* all = new Combined(pc);
+  pc.addElements(all);
+  EXPECT_FLOAT_EQ(209.99998, pc.CalculateResistance(p));
+  delete r1;
+  delete c1;
+  delete i1;
+  delete all;
 }
